d07506f4d617ceb814eeb0728df87c887b397fea
MeetPatel1706 <meetnp1706@gmail.com>
2022-06-30 15:09:09 +0530

    basic commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./components/Main";
+
+ReactDOM.render(
+  <div>
+    <div className="main-container">
+      <Main />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
