9b7310182fbe6390ba3f3d9b32955bd1bef2ec4d
MeetPatel1706 <meetnp1706@gmail.com>
2022-07-26 16:25:20 +0530

    literally no bug!!

--- src/components/bases/Expenses.jsx
+++ src/components/bases/Expenses.jsx
@@ -1,61 +1,201 @@
 import React from "react";
 
 export default function Expenses(props) {
+  const [symbol, setSymbol] = React.useState("$");
   const [expenses, setExpenses] = React.useState([
     {
       id: 1,
-      symbol: "$",
       name: "Rent",
       amount: 1000,
       category: "monthly",
+      date: "01-01-2020",
     },
     {
       id: 2,
-      symbol: "$",
       name: "Milk",
       amount: 100,
       category: "daily",
+      date: "01-02-2020",
     },
   ]);
-  const [categories, setCategories] = React.useState(["monthly", "daily"]);
+  const [dates, setDates] = React.useState(["01-01-2020", "01-02-2020"]);
   const addExpense = (e) => {
-    if (e.target.value !== "" && e.keyCode === 13) {
+    if (
+      e.key === "Enter" &&
+      e.target.value !== "" &&
+      e.target.value.slice(e.target.value.indexOf(" ")).length > 1 &&
+      e.target.value.slice(1, e.target.value.indexOf(" ")) !== "" &&
+      parseFloat(
+        parseFloat(
+          e.target.value.slice(symbol.length, e.target.value.indexOf(" ") + 1)
+        ).toFixed(2)
+      ) > 0
+    ) {
       setExpenses([
         ...expenses,
         {
           id: expenses.length + 1,
-          symbol: e.target.value.slice(0, 0),
-          name: e.target.value.slice(e.target.value.indexOf(" ")),
-          amount: parseInt(
-            e.target.value.slice(1, e.target.value.indexOf(" "))
+          name: e.target.value.slice(e.target.value.indexOf(" ") + 1),
+          amount: parseFloat(
+            parseFloat(
+              e.target.value.slice(
+                symbol.length,
+                e.target.value.indexOf(" ") + 1
+              )
+            ).toFixed(2)
           ),
           category: "other",
+          date: `${new Date().getDate().toString().padStart(2, "0")}-${(
+            new Date().getMonth() + 1
+          )
+            .toString()
+            .padStart(2, "0")}-${new Date().getFullYear()}`,
         },
       ]);
-      console.log(1);
+      if (
+        !dates.includes(
+          `${new Date().getDate().toString().padStart(2, "0")}-${(
+            new Date().getMonth() + 1
+          )
+            .toString()
+            .padStart(2, "0")}-${new Date().getFullYear()}`
+        )
+      ) {
+        setDates([
+          ...dates,
+          `${new Date().getDate().toString().padStart(2, "0")}-${(
+            new Date().getMonth() + 1
+          )
+            .toString()
+            .padStart(2, "0")}-${new Date().getFullYear()}`,
+        ]);
+      }
     }
+  };
+  const returnTotalOfDate = (date) => {
+    let total = 0;
+    expenses.forEach((expense) => {
+      if (expense.date === date) {
+        total += expense.amount;
+      }
+    });
+    return total;
+  };
+  const deleteExpense = (id) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
+  const renameExpense = (e, expenseid) => {
+    if (
+      e.key === "Enter" &&
+      e.target.value !== "" &&
+      e.target.value.slice(e.target.value.indexOf(" ")).length > 1 &&
+      e.target.value.slice(1, e.target.value.indexOf(" ")) !== "" &&
+      parseFloat(
+        parseFloat(
+          e.target.value.slice(symbol.length, e.target.value.indexOf(" ") + 1)
+        ).toFixed(2)
+      ) > 0
+    ) {
+      console.log(
+        parseFloat(
+          parseFloat(
+            e.target.value.slice(symbol.length, e.target.value.indexOf(" ") + 1)
+          ).toFixed(2)
+        )
+      );
+      setExpenses(
+        expenses.map((expense) => {
+          expenseid === expense.id &&
+            (expense.amount = parseFloat(
+              parseFloat(
+                e.target.value.slice(
+                  symbol.length,
+                  e.target.value.indexOf(" ") + 1
+                )
+              ).toFixed(2)
+            ));
+          expenseid === expense.id &&
+            (expense.name = e.target.value.slice(
+              e.target.value.indexOf(" ") + 1
+            ));
+          return expense;
+        })
+      );
+    }
+  };
+  const returnExpensesOfDate = (date) => {
+    return expenses.filter((expense) => expense.date === date);
   };
   return (
     <div>
       <h1>Expenses</h1>
       <div>
         <input
+          type="text"
+          placeholder={"Set Currency..."}
+          maxLength="3"
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && e.target.value !== "") {
+              setSymbol(e.target.value);
+            }
+          }}
+        />
+        <br />
+        <input
+          onFocus={(e) =>
+            e.target.value === "" ? (e.target.value = symbol) : null
+          }
+          onBlur={(e) => (e.target.value = "")}
           type={"text"}
-          onChange={(e) => addExpense(e)}
-          placeholder={"$2000 elon musk"}
+          onKeyDown={(e) => addExpense(e)}
+          placeholder={symbol + "2000 elon musk"}
         />
       </div>
-      <div>
-        <table>
-          {expenses.map((expense) => (
-            <tr key={expense.id}>
-              <td>{expense.symbol}</td>
-              <td>{expense.amount}</td>
-              <td>{expense.name}</td>
-            </tr>
-          ))}
-        </table>
-      </div>
+      {dates.map((date) => (
+        <div key={date}>
+          <h3>{date}</h3>
+          <div>
+            <table>
+              {returnExpensesOfDate(date).map((expense) => (
+                <tbody key={expense.id}>
+                  <tr key={date.indexOf(expense.date)}>
+                    <td>{symbol}</td>
+                    <td>{expense.amount}</td>
+                    <td>{expense.name}</td>
+                    <td>
+                      <button onClick={() => deleteExpense(expense.id)}>
+                        Delete
+                      </button>
+                      <input
+                        onFocus={(e) =>
+                          e.target.value === ""
+                            ? (e.target.value = symbol + expense.amount + " ")
+                            : null
+                        }
+                        onBlur={(e) => (e.target.value = "")}
+                        type={"text"}
+                        onKeyDown={(e) => renameExpense(e, expense.id)}
+                        placeholder={
+                          symbol + expense.amount + " " + expense.name
+                        }
+                      />
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+              <tfoot>
+                <tr>
+                  <td style={{ borderTop: "1px solid black" }}>{symbol}</td>
+                  <td style={{ borderTop: "1px solid black" }}>
+                    {returnTotalOfDate(date)}
+                  </td>
+                  <td style={{ borderTop: "1px solid black" }}>Total</td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
